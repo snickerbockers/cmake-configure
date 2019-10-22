34c25d17135428976b13963a8e5e3b1b0b277fd1
snickerbockers <snickerbockers@washemu.org>
2019-10-22 03:35:09 -0700

    run cmake in current working directory

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
+const { exec } = require('child_process');
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
+  try {
+      exec("cmake .", (err, stdout, stderr) => {
+	  console.log("cmake configuration complete\n");
+      }) ;
   } 
   catch (error) {
     core.setFailed(error.message);
